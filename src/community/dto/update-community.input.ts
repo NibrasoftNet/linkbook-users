36dd11be4564e0d8c7174de0ed87199d841abe3d
SofienRogue <sofien.abidi@digitalroguewave.com>
2024-02-26 09:31:51 +0100

    create community

--- src/community/dto/update-community.input.ts
+++
@@ -1,8 +0,0 @@
-import { CreateCommunityInput } from './create-community.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
-
-@InputType()
-export class UpdateCommunityInput extends PartialType(CreateCommunityInput) {
-  @Field(() => Int)
-  id: number;
-}
